da41d7f385686a7d74791e230d3cc9b5cb6bfd39
Alok-Sharma <alok.sharma127@gmail.com>
2012-09-30 20:52:37 +0530

    Testing

--- src/com/hackathon/gdg/resq/MainActivity.java
+++ src/com/hackathon/gdg/resq/MainActivity.java
@@ -2,6 +2,7 @@ package com.hackathon.gdg.resq;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Log.d("testing","testing");
     }
 
     @Override
