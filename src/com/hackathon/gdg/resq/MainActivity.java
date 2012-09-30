b37d7c64bcc0cd84aeee549101bfa8bb067b2aed
minedrone <rishabhmakharia@gmail.com>
2012-09-30 21:01:20 +0530

    rishabh

--- src/com/hackathon/gdg/resq/MainActivity.java
+++ src/com/hackathon/gdg/resq/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         Log.d("testing","testing");
+        Log.d("rishabh","testing");
     }
 
     @Override
