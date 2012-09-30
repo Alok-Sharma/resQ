64b68ca8529e2be2eec4c7b9111575968a6f9933
arpavan <arpavan1990@gmail.com>
2012-09-30 20:58:38 +0530

    I just tested git-push from my comp...

--- src/com/hackathon/gdg/resq/MainActivity.java
+++ src/com/hackathon/gdg/resq/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        /* Pavan wrote this line! */
+        
         Log.d("testing","testing");
     }
 
