5d80f53ffb7d4a9ca48c345959f7ea9d428e46f8
Alok-Sharma <alok.sharma127@gmail.com>
2012-10-09 08:49:38 +0530

    UI overhaul complete. Woohoo.

--- src/com/hackathon/gdg/resq/MainActivity.java
+++ src/com/hackathon/gdg/resq/MainActivity.java
@@ -48,30 +48,7 @@ public class MainActivity extends Activity {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fixed_tabs);
-		
-		
-//		LayoutInflater inflater = this.getLayoutInflater();
-//		v = inflater.inflate(R.layout.layout_0, null);
-//		submitbutton = (Button)v.findViewById(R.id.submit);
-//		smskeytext = (EditText)v.findViewById(R.id.smskey);
-//		smstextview = (TextView)v.findViewById(R.id.smstext);
-//		emailtext = (EditText)v.findViewById(R.id.emailcontacts);
-//		emailtextview = (TextView)v.findViewById(R.id.emailtext);
-//		afterkeytext = (EditText)v.findViewById(R.id.afterkey);
-//		aftertext = (TextView)v.findViewById(R.id.aftertext);
-//
-//		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-//		final String smsfrompref = prefs.getString("sms",null);
-//		String emailfrompref = prefs.getString("email",null);
-//		isLost = prefs.getBoolean("isLost",false);
-//		Log.d("alok","in oncreate "+isLost);
-//		
-//		if(smsfrompref!=null && emailfrompref!=null){
-//
-//			smskeytext.setText(smsfrompref);
-//			emailtext.setText(emailfrompref);
-//		}
-		
+	
 		testlist.add("Log Entries");testlist.add("Will fill up here");
 		testlist.add("Similar to how it is now.");
 		
@@ -80,75 +57,7 @@ public class MainActivity extends Activity {
 		mFixedTabsAdapter = new FixedTabsAdapter(this);
 		mFixedTabs.setAdapter(mFixedTabsAdapter);
 		mFixedTabs.setViewPager(mPager);
-		
-//		submitbutton.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View arg0) {
-//				if(!isLost){
-//					SharedPreferences.Editor editor = prefs.edit();
-//					if(smskeytext.getText().toString().contains(" "))
-//					{
-//						Toast.makeText(getApplicationContext(),"No spaces allowed in key", Toast.LENGTH_SHORT).show();
-//					}
-//					else
-//					{
-//						editor.putString("sms", smskeytext.getText().toString().replace(" ", ""));
-//						editor.putString("email", emailtext.getText().toString());
-//						editor.commit();
-//						Toast.makeText(getApplicationContext(),"Saved", Toast.LENGTH_SHORT).show();
-//						changeVis();
-//					}
-//				}else{
-//					String checkstring = afterkeytext.getText().toString();
-//					SharedPreferences.Editor editor = prefs.edit();
-//					if(checkstring.equals(smsfrompref) || smsfrompref==null){
-//						Log.d("alok","in if");
-//						isLost = false;
-//						editor.putBoolean("isLost",false);
-//						changeVis();
-//					}else{
-//						Log.d("alok","in else");
-//						isLost = true;
-//						changeVis();
-//					}
-//					editor.commit();
-//				}
-//			}
-//		}); 
 	}
-	
-
-	@Override
-	public void onResume(){
-		super.onResume();
-//		changeVis();
-	}
-
-	/*
-	 * A convenenient method for changing the visibility
-	 * of the appropriate button and textviews based upon
-	 * locked status of phone.
-	 */
-//	public void changeVis(){
-//		Log.d("alok","in changevis "+isLost);
-//		if(isLost){
-//			smskeytext.setText("hurr");
-//			smskeytext.setVisibility(View.INVISIBLE);
-//			smstextview.setVisibility(View.INVISIBLE);
-//			emailtext.setVisibility(View.INVISIBLE);
-//			emailtextview.setVisibility(View.INVISIBLE);
-//			afterkeytext.setVisibility(View.VISIBLE);
-//			aftertext.setVisibility(View.VISIBLE);
-//		}else{
-//			smskeytext.setText("hurr");
-//			smskeytext.setVisibility(View.VISIBLE);
-//			smstextview.setVisibility(View.VISIBLE);
-//			emailtext.setVisibility(View.VISIBLE);
-//			emailtextview.setVisibility(View.VISIBLE);
-//			afterkeytext.setVisibility(View.INVISIBLE);
-//			aftertext.setVisibility(View.INVISIBLE);
-//		}
-//	}
 	
 	/*
 	 * Initializes the ViewPager (The content below the tabs).
